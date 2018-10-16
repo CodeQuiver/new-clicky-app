11238b7dced6080f989a1c6f8c2dd7f7595eca3b
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-10-15 21:10:57 -0400

    updated to add images and render three squares using an array at the Board state level to manage their display values and track clicks rather than tracking them at the individual element level. Later when we have to shuffle the order, we will shuffle that array rather than the Square elements themselves.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
 
 //construct Square
-function Square() {
+class Square extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     imgValue: 'https://randomuser.me/api/portraits/men/54.jpg',
+  //     isClicked: false,
+  //   };
+  // }
 
-        return (
-            <button>
-                X
-            </button>
-        )
+  render(props) {
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({isClicked:true})} //replace with a handleclick function
+      >
+        <img src={this.props.value.imgValue} alt="headshot of smiling person" width="150px" height="auto" />
+      </button>
+    );
+  }
 }
+// function Square() {
+
+//         return (
+//             <button>
+//                 X
+//             </button>
+//         )
+// }
 //create array of state objects
 //call square constructor with array of state objects
 //render each square to DOM
 
 
 class Board extends Component {
+
+  //constructor for tracking the state of the board, including the squares' current values
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: [
+        {
+          imgValue: 'https://randomuser.me/api/portraits/men/54.jpg',
+          isClicked: false
+        },
+        {
+          imgValue: 'https://randomuser.me/api/portraits/men/54.jpg',
+          isClicked: false
+        },
+        {
+          imgValue: 'https://randomuser.me/api/portraits/men/54.jpg',
+          isClicked: false
+        }
+      ]
+    };
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} />;
+  }
+
   render() {
     return (
       <section className="Board component-wrapper">
         This is the Board Component!
-        <Square />
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        {/* <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div> */}
       </section>
     );
   }
